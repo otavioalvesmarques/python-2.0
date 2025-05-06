43135ae659a52db937b5c8ab74e896c4151c2121
otavio2007 <otavio.alves.marques@escola.pr.gov.br>
2025-05-06 12:28:51 -0300

    commit inicial

---
+++ pythonProject/capitulo 2/exemplo2nomedevariveis.py
@@ -0,0 +1,12 @@
+## variaveis não começam com numeros
+##76trombones = 'big parentes'
+
+#variavesis tambem não podem ter caracteres especiais
+#tirando o ____
+
+n = 10
+m = 25
+
+total = n + m
+
+print(total)
