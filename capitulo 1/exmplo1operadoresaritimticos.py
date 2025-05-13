8a6f4edbb6fe69055c4b82f699f7ce27af95f08c
otavio <otavio.alves.marques@escola.pr.gov.br>
2025-05-13 11:33:19 -0300

    operadores aritimeticos commit

---
+++ capitulo 1/exmplo1operadoresaritimticos.py
@@ -0,0 +1,19 @@
+# operador de soma
+
+soma = 1 + 2
+
+# operador de subtração
+
+subtracao = 2 - 1
+
+# operador de multiplicacao
+
+multiplicacao = 2 * 1
+
+# operador de divisão
+
+divisao = 2/1
+
+ # operador de exponencial
+
+potencia = 2 ** 4
