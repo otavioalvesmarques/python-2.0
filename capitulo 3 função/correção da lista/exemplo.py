70d119e4b3e5b4e33572e99ca70778b750f1d688
OTAVIO ALVES MARQUES <otavio.alves.marques@escola.pr.gov.br>
2025-09-09 11:50:52 -0300

    commit
    commit

---
+++ capitulo 3 função/correção da lista/exemplo.py
@@ -0,0 +1,8 @@
+def soma(nome, nome2):
+    print("o seu nome inteiro é:", nome + nome2 )
+
+
+nome = str(input(" digite seu nome:"))
+nome2 = str(input(" digite seu sobrenome: "))
+
+soma(nome, nome2)
